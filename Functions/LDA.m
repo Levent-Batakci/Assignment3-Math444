44956ad4d6091435db01ff987bf4a1feb5015186
lab192 <lab192@case.edu>
2021-03-05 00:25:51 -0500

    Basics Implemented, NOT FUNCTIONAL -LDA currently gives garbage results :(

---
+++ Functions/LDA.m
@@ -0,0 +1,44 @@
+function Q = LDA(X, I, k)
+%LDA returns the leading directions to project the data
+
+    %Get the dimensions
+    [n, p] = size(X);
+    P = zeros(1,k); %Number of elements per cluster
+    for i = 1:k
+        P(1,i) = nnz(I == i);
+    end
+
+    %Compute the group centers
+    C = getCenters(X, I, k);
+    c0 = sum(X, 2) / p; %Get the overall center
+    Xc = X - c0 * ones(1, p);
+    
+    %Compute the within cluster scatter
+    S = zeros(n, n, k);
+    Sw = zeros(n, n); %Within cluster scatter
+    for i = 1:k
+        for x = X(:, (I==i))
+            c_ = x - C(1,i); 
+            S(:,:, i) = S(:,:, i) + c_*(c_');
+        end
+        Sw = Sw + S(:,:,i);
+    end
+    
+    %Adjust Sw
+    [U, D, V] = svd(Sw);
+    d1 = D(1,1);
+    eps = 10e-10 * d1;
+    Sw = U * diag(diag(D) + eps) * V';
+    %Sw = Sw + eps * eye(n); 
+    
+    %Compute Sb, the between-cluster scatter matrix
+    Sb = Xc * Xc';
+    
+    %Compute the Cholesky factorization of Sw,eps
+    K = chol(Sw);
+    
+   [W, D] = eigs(inv(K') * Sb * inv(K), k-1);
+    
+   Q = K \ W;
+end
+
---
+++ Tester.m
@@ -0,0 +1,26 @@
+%Levent Batakci
+%Test the stuff !
+
+%Parameters
+k=3;
+
+%Generate data
+c1 = [-3 -3 -3]' * ones(1, 200);
+c2 = [2 0 0]' * ones(1, 200);
+c3 = [4 4 -6]' * ones(1, 200);
+dev = [1 1 1]' * ones(1, 200);
+X = [normrnd(c1, dev) normrnd(c2, dev) normrnd(c3, dev)];
+I = [ones(1, 200) 2*ones(1, 200) 3*ones(1, 200)];
+
+Q = LDA(X, I, k)
+
+Z = Q' * X;
+
+figure(1)
+p = 200;
+scatter(Z(1,1:p), Z(2,1:p), 'r');
+scatter(Z(1,p+1:2*p), Z(2,p+1:2*p), 'g');
+scatter(Z(1,2*p+1:3*p), Z(2,2*p+1:3*p), 'b');
+
+figure(2)
+scatter3(X(1,:), X(2,:), X(3,:));
